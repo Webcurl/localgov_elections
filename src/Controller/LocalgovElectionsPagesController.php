41094de5377418297c6802f050ad3c570c49575b
Graham Cole <graham.cole@webcurl.co.uk>
2022-03-25 16:43:03 +0000

    Political make-up chart & contest status handling

--- src/Controller/LocalgovElectionsPagesController.php
+++ src/Controller/LocalgovElectionsPagesController.php
@@ -28,24 +28,44 @@ class LocalgovElectionsPagesController extends ControllerBase {
     $candidate_vids = [];
     $seat_ids = [];
     $seat_vids = [];
+    $undeclared_contest_seat_ids = [];
+    $undeclared_contest_seat_vids = [];
+
     foreach ($contests as $contest) {
-      $candidate_ids = array_merge($candidate_ids, array_column($contest->field_candidates->getValue(), 'target_id'));
-      $candidate_vids = array_merge($candidate_vids, array_column($contest->field_candidates->getValue(), 'target_revision_id'));
-      $seat_ids = array_merge($seat_ids, array_column($contest->field_previous_seat_makeup->getValue(), 'target_id'));
-      $seat_vids = array_merge($seat_vids, array_column($contest->field_previous_seat_makeup->getValue(), 'target_revision_id'));
+
+      $contest_state = $contest->get('moderation_state')->getString();
+
+      $contest_seat_ids = array_column($contest->field_previous_seat_makeup->getValue(), 'target_id');
+      $contest_seat_vids = array_column($contest->field_previous_seat_makeup->getValue(), 'target_revision_id');
+
+      // Always process unchanged seats.
+      $seat_ids = array_merge($seat_ids, $contest_seat_ids);
+      $seat_vids = array_merge($seat_vids, $contest_seat_vids);
+
+      if ($contest_state == 'declared') {
+        $candidate_ids = array_merge($candidate_ids, array_column($contest->field_candidates->getValue(), 'target_id'));
+        $candidate_vids = array_merge($candidate_vids, array_column($contest->field_candidates->getValue(), 'target_revision_id'));
+      }
+      else {
+        $undeclared_contest_seat_ids = array_merge($undeclared_contest_seat_ids, $contest_seat_ids);
+        $undeclared_contest_seat_vids = array_merge($undeclared_contest_seat_vids, $contest_seat_vids);
+      }
     }
 
-    $elected_candidates = \Drupal::entityQueryAggregate('localgov_elections_candidate')
-      ->condition('field_elected', TRUE)
-      ->condition('id', $candidate_ids, 'IN')
-      ->condition('id', $candidate_vids, 'IN')
-      ->groupBy('field_party')
-      ->aggregate('id', 'COUNT')
-      ->execute();
+    $elected_candidates = [];
+    if (!empty($candidate_ids)) {
+      $elected_candidates = \Drupal::entityQueryAggregate('localgov_elections_candidate')
+        ->condition('field_elected', TRUE)
+        ->condition('id', $candidate_ids, 'IN')
+        ->condition('id', $candidate_vids, 'IN')
+        ->groupBy('field_party')
+        ->aggregate('id', 'COUNT')
+        ->execute();
+    }
 
-    $existing_seats = [];
+    $declared_existing_seats = [];
     if (!empty($seat_ids)) {
-      $existing_seats = \Drupal::entityQueryAggregate('paragraph')
+      $declared_existing_seats = \Drupal::entityQueryAggregate('paragraph')
         ->condition('field_contested', FALSE)
         ->condition('id', $seat_ids, 'IN')
         ->condition('revision_id', $seat_vids, 'IN')
@@ -54,11 +74,23 @@ class LocalgovElectionsPagesController extends ControllerBase {
         ->execute();
     }
 
+    $contested_seats_count = 0;
+    if (!empty($undeclared_contest_seat_ids)) {
+      $contested_seats_result = \Drupal::entityQueryAggregate('paragraph')
+        ->condition('field_contested', TRUE)
+        ->condition('id', $undeclared_contest_seat_ids, 'IN')
+        ->condition('revision_id', $undeclared_contest_seat_vids, 'IN')
+        ->aggregate('id', 'COUNT')
+        ->execute();
+
+      $contested_seats_count = $contested_seats_result[0]['id_count'];
+    }
+
 
     // Join together uncontested seats and newly elected candidates
     $council_makeup = [];
 
-    foreach ([$elected_candidates, $existing_seats] as $group) {
+    foreach ([$elected_candidates, $declared_existing_seats] as $group) {
       foreach ($group as $seat) {
         $party = $seat['field_party_target_id'];
         $council_makeup[$party] = isset($council_makeup[$party]) ? $council_makeup[$party] += $seat['id_count'] : $seat['id_count'];
@@ -69,19 +101,71 @@ class LocalgovElectionsPagesController extends ControllerBase {
       ->getStorage('taxonomy_term')
       ->loadMultiple(array_keys($council_makeup));
 
-    $council_makeup = array_map(function ($party_tid, $seats) use ($party_terms) {
-      $party = isset($party_terms[$party_tid]) ? $party_terms[$party_tid]->label() : "Others";
+    $table_rows = array_map(function ($party_tid, $seats) use ($party_terms) {
+
       return array(
-        'party' => $party,
-        'seats' => $seats
+        'color' => isset($party_terms[$party_tid]) ? ['data' => $party_terms[$party_tid]->get('field_color')->view(['type' => 'color_field_formatter_swatch', 'label' => 'hidden'])] : '',
+        'party' => isset($party_terms[$party_tid]) ? $party_terms[$party_tid]->label() : "Others",
+        'seats' => $seats,
       );
     }, array_keys($council_makeup), $council_makeup);
 
+    if ($contested_seats_count > 0) {
+      $table_rows[] = [
+        'color' => '',
+        'party' => $this->t('Undeclared'),
+        'seats' => $contested_seats_count,
+      ];
+    }
 
     $build['content'] = [
       '#type' => 'table',
-      '#header' => ['Party', 'Seats'],
-      '#rows' => $council_makeup,
+      '#header' => [
+        ['data' => $this->t('Party'), 'colspan' => 2],
+        'Seats'
+      ],
+      '#rows' => $table_rows,
+    ];
+
+    $chart_data = array_map(function ($party_tid, $seats) use ($party_terms) {
+      return array(
+        'color' => isset($party_terms[$party_tid]) ? $party_terms[$party_tid]->get('field_color')->color : '#ccc',
+        'party' => isset($party_terms[$party_tid]) ? $party_terms[$party_tid]->label() : "Others",
+        'seats' => $seats,
+      );
+    }, array_keys($council_makeup), $council_makeup);
+
+    if ($contested_seats_count > 0) {
+      $chart_data[] = [
+        'color' => '#ccc',
+        'party' => $this->t('Undeclared')->render(),
+        'seats' => $contested_seats_count,
+      ];
+    }
+
+    $series = [
+      '#type' => 'chart_data',
+      '#title' => $this->t('Seats'),
+      '#data' => array_column($chart_data, 'seats'),
+      '#color' => array_column($chart_data, 'color'), // This gets overwritten by global colors in Charts 5.x-dev :-/
+    ];
+
+    $xaxis = [
+      '#type' => 'chart_xaxis',
+      '#title' => $this->t('Party'),
+      '#labels' => array_column($chart_data, 'party'),
+    ];
+
+    $build['makeup_chart'] = [
+      '#type' => 'chart',
+      '#chart_type' => 'pie',
+      '#title' => $this->t('Council Make-up'),
+      '#data_labels' => TRUE,
+      //'#colors' => array_column($council_makeup, 'color'),
+      '#legend' => TRUE,
+      '#legend_position' => 'top',
+      'series' => $series,
+      'x_axis' => $xaxis,
     ];
 
     return $build;
