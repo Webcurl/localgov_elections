19e778b816cace38cabad0637bfdbf22d1b94ad8
Graham Cole <graham.cole@webcurl.co.uk>
2022-03-28 11:23:40 +0100

    Block access to turnout / spoiled totals until contest declared

--- src/ElectionsContestAccessControlHandler.php
+++ src/ElectionsContestAccessControlHandler.php
@@ -5,6 +5,8 @@ namespace Drupal\localgov_elections;
 use Drupal\Core\Access\AccessResult;
 use Drupal\Core\Entity\EntityAccessControlHandler;
 use Drupal\Core\Entity\EntityInterface;
+use Drupal\Core\Field\FieldDefinitionInterface;
+use Drupal\Core\Field\FieldItemListInterface;
 use Drupal\Core\Session\AccountInterface;
 
 /**
@@ -32,6 +34,45 @@ class ElectionsContestAccessControlHandler extends EntityAccessControlHandler {
         return AccessResult::neutral();
     }
 
+  }
+
+  /**
+   * Prevent access to certain fields before contest result declared.
+   *
+   * @param string $operation
+   * @param FieldDefinitionInterface $field_definition
+   * @param AccountInterface $account
+   * @param FieldItemListInterface|null $items
+   * @return AccessResult|\Drupal\Core\Access\AccessResultAllowed|\Drupal\Core\Access\AccessResultForbidden|\Drupal\Core\Access\AccessResultInterface|\Drupal\Core\Access\AccessResultNeutral|\Drupal\Core\Access\AccessResultReasonInterface
+   */
+  protected function checkFieldAccess($operation, FieldDefinitionInterface $field_definition, AccountInterface $account, FieldItemListInterface $items = NULL)
+  {
+
+    if (!$items) {
+      // Allow access to field in principle. (doesn't bother checking individual items if this is neutral)
+      return AccessResult::allowed();
+    }
+
+    $results_fields = [
+      'field_turnout',
+      'field_turnout_percentage',
+      'field_rejected_papers'
+    ];
+
+    if (in_array($field_definition->getName(), $results_fields)) {
+
+      $contest = $items->getEntity();
+      $contest_state = $contest->get('moderation_state')->getString();
+      $permissionCheck = AccessResult::allowedIfHasPermission($account, 'administer elections')
+        ->cachePerPermissions();
+
+      return $permissionCheck
+        ->orIf(
+          AccessResult::allowedIf($contest_state == 'declared')->addCacheableDependency($contest)
+        );
+    }
+
+    return AccessResult::allowed();
   }
 
   /**
